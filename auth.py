6b583be850e1b84c4fc069c501560d4a8b727f30
vulpes0zedra <vulpes0zedra@gmail.com>
2022-03-28 00:55:43 +0900

    Store access token to db

--- auth.py
+++ auth.py
@@ -6,16 +6,15 @@ from dateutil.relativedelta import relativedelta
 
 import jwt
 import requests
-from pymongo import MongoClient
 from flask import Flask, request, jsonify, abort
 from flask_restx import Api, Resource, Namespace, reqparse, fields
 
 from common import *
-from db_connect import FungeDAO
 from env.kakao import *
+from db_connect import FungeDAO
 
 
-DAO = FungeDAO()
+db = FungeDAO()
 Auth = Namespace(name='Auth')
 
 id_token = Auth.model('ID_TOKEN', {'Authorization': fields.String()})
@@ -23,6 +22,7 @@ id_token = Auth.model('ID_TOKEN', {'Authorization': fields.String()})
 def get_kakao_token(auth):
     parameter = {'grant_type': 'authorization_code', 'client_id': KKO_REST_KEY, 'redirect_uri': f'{SERVICE_URL}:{SERVICE_PORT}/auth/kakao', 'code': auth}
     response = requests.post(f'{KAKAO_AUTH}/oauth/token', data=parameter).json()
+    print(response)
     if 'access_token' in response:
         return response
     else:
@@ -31,11 +31,11 @@ def get_kakao_token(auth):
 
 
 def get_public_key(kid):
-    keys = list(DAO.find('kkoPublicKey', {}))
+    keys = list(db.find('kkoPublicKey', {}))
     if not keys:
         keys = requests.get(f'{KAKAO_AUTH}/.well-known/jwks.json').json()['keys']
         keys = [dict(x, **{'expireAt': datetime.datetime.utcnow() + relativedelta(months=+1)}) for x in keys]
-        DAO.insert_many('kkoPublicKey', keys)
+        db.insert_many('kkoPublicKey', keys)
 
     return list(filter(lambda x: x['kid'] == kid, keys))[0]
 
@@ -57,7 +57,7 @@ def verify_token(token):
             raise
 
 def is_valid_token(id_token):
-    payload = id_token.split('.')
+    payload = id_token.split('.')[1]
     decoded_payload = json.loads(base64.b64decode(payload))
 
     if decoded_payload['iss'] != KAKAO_AUTH:
@@ -70,23 +70,68 @@ def is_valid_token(id_token):
         return True
 
 
+def set_access_token(uid, access_token):
+    db.find_one_and_update('accessToken', {'id': uid}, {'$set': {'id': uid, 'access_token': access_token, 'createdAt': datetime.datetime.utcnow()}}, upsert=True)
+
+
+def get_access_token(uid):
+    access_token = db.find_one('accessToken', {'id': uid})
+    if not access_token:
+        access_token = refresh_access_token(uid)
+        set_access_token(uid, access_token)
+    return access_token
+
+
+def set_refresh_token(uid, refresh_token):
+    db.find_one_and_update('refreshToken', {'id': uid}, {'$set': {'id': uid, 'refresh_token': refresh_token, 'createdAt': datetime.datetime.utcnow()}}, upsert=True)
+
+
+def get_refresh_token(uid):
+    refresh_token = db.find_one('refreshToken', {'id': uid})
+    return refresh_token if refresh_token else False
+
+
+def refresh_access_token(uid):
+    refresh_token = get_refresh_token(uid)
+    if not refresh_token:
+        False
+
+    parameter = {'grant_type': 'refresh_token', 'client_id': KKO_REST_KEY, 'refresh_token': refresh_token}
+    response = requests.post(f'{KAKAO_AUTH}/oauth/token', data=parameter).json()
+    print(response)
+    
+    set_access_token(uid, response['access_token'])
+    if 'refresh_token' in response.keys():
+        set_refresh_token(uid, response['refresh_token'])
+
+
+def set_id_token(uid, id_token):
+    db.find_one_and_update('idToken', {'id': uid}, {'$set': {'id': uid, 'id_token': id_token, 'createdAt': datetime.datetime.utcnow()}}, upsert=True)
+
+
+
+def signup(id_token):
+    pass
+
+
 @Auth.route('/kakao')
 class GetAuthorization(Resource):
     @Auth.marshal_with(id_token)
     def get(self):
         try:
             token = get_kakao_token(request.args['code'])
-            if not token or not is_valid_token(token['id_token']):
+            id_token = verify_token(token['id_token'])
+            if not token:
+                return abort(400, 'Token is not issued.')
+            elif not is_valid_token(token['id_token']):
                 return abort(400, 'Token is invalid.')
-            if not token or not verify_token(token['id_token']):
+            elif not id_token:
                 return abort(401, 'Failed to verify your token.')
             else:
+                set_access_token(id_token['sub'], token['access_token'])
+                set_refresh_token(id_token['sub'], token['refresh_token'])
+                signup(id_token)
                 return {'Authorization': f'Bearer {token["id_token"]}'}, 200
         except Exception:
             raise
 
-
-@Auth.route('/signup')
-class Singup(Resource):
-    def post(self):
-        pass
--- db_connect.py
+++ db_connect.py
@@ -6,11 +6,17 @@ from env.db_config import *
 class FungeDAO:
     def __init__(self):
         self.DB_NAME = 'funge'
-        self.mongo = MongoClient(host='localhost', port=db_config['port'], username=db_config['username'], password=db_config['password'], authSource=self.DB_NAME)[self.DB_NAME]
+        self.mongo = MongoClient(host=db_config['host'], port=db_config['port'], username=db_config['username'], password=db_config['password'], authSource=self.DB_NAME)[self.DB_NAME]
 
         
     def find(self, collection, query):
         return self.mongo[collection].find(query)
+
+    def find_one(self, collection, query):
+        return self.mongo[collection].find_one(query)
+
+    def find_one_and_update(self, collection, query, update, sort=None, upsert=False):
+        return self.mongo[collection].find_one_and_update(query, update, sort=sort, upsert=upsert)
 
     def insert_one(self, collection, document):
         return self.mongo[collection].insert_one(document)
