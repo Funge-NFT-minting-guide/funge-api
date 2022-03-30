0b5257ffcb9895792dc3b5a92d0dc7beae798dd0
vulpes0zedra <vulpes0zedra@gmail.com>
2022-03-30 20:06:14 +0900

    add to exception code

--- account.py
+++ account.py
@@ -5,7 +5,7 @@ from flask_restx import Api, Resource, Namespace, fields
 from common import *
 from env.kakao import *
 from db_connect import FungeDAO
-from verify import authenticate_user
+from verify import verify_token, authenticate_user
 
 
 db = FungeDAO()
@@ -14,12 +14,24 @@ Account = Namespace(name='Account')
 @Account.route('/profile')
 class User(Resource):
     def get(self):
-        id_token = request.headers['Authorization'].split(' ')[1]
-        verified = authenticate_user(id_token)
-        if not verified: 
+        try:
+            _type, id_token = request.headers['Authorization'].split(' ')
+            if _type != 'Bearer':
+                return abort(400, f'{_type} type is not allowed.')
+            if not id_token:
+                raise KeyError
+        except KeyError:
+            return abort(400, 'Token is needed.')
+        except IndexError:
+            return abort(400, 'Token is not valid.')
+
+        authed_token = authenticate_user(id_token)
+        if not authed_token:
             return abort(401, 'Unauthorized')
 
-        response = db.find_one('users', {'id': verified['sub']})
+        verified_token = verify_token(id_token)
+
+        response = db.find_one('users', {'id': verified_token['sub']})
         user = {k: v for k, v in response.items() if k in ['nickname', 'email', 'profile_image_url']}
         return user
 
