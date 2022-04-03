ad2cc82fe69b22a8d046b07e8130b7d883204d81
vulpes0zedra <vulpes0zedra@gmail.com>
2022-04-03 14:18:42 +0900

    Implements minting related apis except for the search filter.

--- common.py
+++ common.py
@@ -2,6 +2,10 @@ SERVICE_URL = 'https://funge.kr'
 DEVELOP_URL = 'http://localhost:8000'
 SERVICE_HOST = '0.0.0.0'
 SERVICE_PORT = 8000
+
+TWITTY_URL = 'http://localhost:3000'
+ADMIN_DEV_URL = 'http://localhost:9000'
+
 SERVICE_ROOT = 'app'
 UPLOAD_PATH = 'uploads'
 UPLOAD_USERS = 'users'
@@ -20,3 +24,4 @@ ERR_TOKEN_INVALID = (400, 'Token is not valid.')
 ERR_UNAUTHORIZED = (401, 'Unauthorized.')
 ERR_TOKEN_EXPIRED = (412, 'Token has expired.')
 ERR_NOT_EXPECTED = (400, 'Payload is not expected.')
+ERR_NOT_FOUND = (404, 'No result found.')
--- minting.py
+++ minting.py
@@ -1,10 +1,123 @@
-from flask import Flask
-from flask_restx import Api, Resource, Namespace
+import json
+import requests
+
+from flask import request, abort
+from flask_restx import Resource, Namespace, fields, reqparse
+
+from common import *
 
 
 Minting = Namespace(name='Minting')
 
-@Minting.route('/brief/<string:date>')
-class GetMintingBreif(Resource):
-    def get(self, date):
-        return {'project': 'MetaKongz', 'date': '2022-02-27 10:30', 'mint': '500 KLAY / 5 per 1TX'}
+minting_tweet = Minting.model('MintingTweet', {
+    '_id': fields.String(readonly=True),
+    'id': fields.String(readonly=True),
+    'created_at': fields.DateTime(),
+    'text': fields.String(),
+    'user': fields.String(),
+    'uid': fields.String(),
+    'profile_image_url': fields.String(),
+    'followers': fields.Integer(),
+    'url': fields.String(),
+    'invalid': fields.Boolean(),
+    'outdated': fields.Boolean(),
+    'processed': fields.Boolean()
+    })
+
+minting_info_detail = Minting.model('MintingInfoDetail', {
+    'mintingType': fields.String,
+    'mintingTime': fields.DateTime,
+    'mintingPrice': fields.Integer,
+    'mintingCurrency': fields.String,
+    'mintingAmount': fields.String
+    })
+
+minting_info = Minting.model('MintingInfo', {
+    '_id': fields.String(readonly=True),
+    'tweetId': fields.String(readonly=True),
+    'profileImageUrl': fields.String(),
+    'followers': fields.Integer(),
+    'url': fields.String(),
+    'projectName': fields.String(),
+    'date': fields.Date(),
+    'site': fields.String(),
+    'etc': fields.String(),
+    })
+
+
+parser = reqparse.RequestParser()
+parser.add_argument('query', type=str, default={})
+parser.add_argument('order', type=int, default=-1, choices=(-1, 1))
+parser.add_argument('offset', type=int, default=0)
+parser.add_argument('limit', type=int, default=10)
+parser.add_argument('flag', type=str, default=None, choices=('invalid', 'outdated', 'processed'))
+parser.add_argument('filter', type=str, choices=('_id', 'tweetId'))
+
+
+@Minting.route('/tweets')
+class MintingTweets(Resource):
+    @Minting.marshal_list_with(minting_tweet)
+    def get(self):
+        args = parser.parse_args()
+        req_payload = f"{TWITTY_URL}/minting/tweets?query={args['query']}&order={args['order']}&offset={args['offset']}&limit={args['limit']}"
+        ret = requests.get(req_payload).json()
+        return ret
+
+
+@Minting.route('/tweets/<string:tid>')
+class MintingTweetsOne(Resource):
+    @Minting.marshal_with(minting_tweet)
+    def get(self, tid):
+        req_payload = f"{TWITTY_URL}/minting/tweets/{tid}"
+        ret = requests.get(req_payload).json()
+        return [ret] if ret else abort(*ERR_NOT_FOUND)
+
+
+    def put(self, tid):
+        args = parser.parse_args()
+        req_payload = f"{TWITTY_URL}/minting/tweets/{tid}?flag={args['flag']}"
+        ret = requests.put(req_payload).json()
+        return [ret] if ret else abort(*ERR_NOT_FOUND)
+
+
+@Minting.route('/info')
+class MintingInfo(Resource):
+    @Minting.marshal_list_with(minting_info)
+    def get(self):
+        req_payload = f"{ADMIN_DEV_URL}/minting/info"
+        ret = requests.get(req_payload).json()
+        return ret
+
+
+    def post(self):
+        req_payload = f"{ADMIN_DEV_URL}/minting/info"
+        ret = requests.post(req_payload, data=request.data).json()
+        return ret
+
+
+@Minting.route('/info/<string:tweet_id>')
+class MintingInfoTid(Resource):
+    def get(self, tweet_id):
+        args = parser.parse_args()
+        req_payload = f"{ADMIN_DEV_URL}/minting/info/{tweet_id}?filter={args['filter']}"
+        ret = requests.get(req_payload)
+        print(ret.json())
+        return ret.json() if ret.status_code == 200 else abort(*ERR_NOT_FOUND)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
